c3f6b03951d5353913b676209a1f4630d58c5d21
Mattral <88831350+Mattral@users.noreply.github.com>
2026-01-02 15:49:41 +0000

    Create flostingPointTypes.rs

---
+++ Day 2/flostingPointTypes.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x: f32 = 3.14;
+    let y: f64 = 2.718281828;
+
+    println!("x = {}, y = {}", x, y);
+}
+
+/*
+Type	Precision
+f32	   single precision
+f64	   double precision (default)
+*/
