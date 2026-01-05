4e68c30415021ab6bd790151ce23a45b2f419467
Mattral <88831350+Mattral@users.noreply.github.com>
2026-01-05 14:55:10 +0000

    Create labeled_loop.rs

---
+++ Day 3/labeled_loop.rs
@@ -0,0 +1,10 @@
+fn main() {
+    'outer: for i in 1..=3 {
+        for j in 1..=3 {
+            if i == 2 && j == 2 {
+                break 'outer;
+            }
+            println!("i = {}, j = {}", i, j);
+        }
+    }
+}
