be2ece42911f0ce9ccb3ac7078f51d76f222b357
Mattral <88831350+Mattral@users.noreply.github.com>
2026-01-05 14:52:12 +0000

    Create else_if.rs

---
+++ Day 3/else_if.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = 0;
+
+    if number > 0 {
+        println!("Positive");
+    } else if number < 0 {
+        println!("Negative");
+    } else {
+        println!("Zero");
+    }
+}
