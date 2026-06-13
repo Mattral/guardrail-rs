f4b6bbd9f83874b3d4d370eef03021067d35f00d
Min Htet Myet <88831350+Mattral@users.noreply.github.com>
2026-06-13 13:34:23 +0900

    fix(file_structure): correct off-by-one in dir structure

--- Day 2/NumericOperaions.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let sum = 5 + 3;
-    let diff = 10 - 4;
-    let product = 6 * 7;
-    let quotient = 10.0 / 3.0;
-    let remainder = 10 % 3;
-
-    println!("{}, {}, {}, {}, {}", sum, diff, product, quotient, remainder);
-}
--- Day 5/tuple.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let person = ("Min", 25, true);
-
-    println!("Name: {}", person.0);
-    println!("Age: {}", person.1);
-    println!("Student: {}", person.2);
-}
